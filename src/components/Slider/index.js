cb1310015476ced69c4946bcc2e9ee015bc6655b
Marian Hello <marian.hello@gmail.com>
2017-11-17 08:16:12 +0100

    Slider component

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,2 +1,33 @@
-import UnimplementedView from '../UnimplementedView';
-export default UnimplementedView;
+import PropTypes from 'prop-types';
+import React from 'react';
+import RCSlider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+const Slider = ({
+  value,
+  step,
+  minimumValue,
+  maximumValue,
+  onSlidingComplete,
+  onValueChange
+}) => (
+  <RCSlider
+    min={minimumValue}
+    max={maximumValue}
+    defaultValue={value}
+    step={step}
+    onChange={onValueChange}
+    onAfterChange={onSlidingComplete}
+  />
+);
+
+Slider.propTypes = {
+  value: PropTypes.number,
+  step: PropTypes.number,
+  minimumValue: PropTypes.number,
+  maximumValue: PropTypes.number,
+  onSlidingComplete: PropTypes.func,
+  onValueChange: PropTypes.func
+};
+
+export default Slider;
