c79dcd4c32edc26ad69bdb7ca2a430d2fc60f8fa
Marian Hello <marian.hello@gmail.com>
2017-11-17 08:34:46 +0100

    Modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,2 +1,19 @@
-import UnimplementedView from '../UnimplementedView';
-export default UnimplementedView;
+import React from 'react';
+import { Portal } from 'react-portal';
+import StyleSheet from '../../apis/StyleSheet';
+import View from '../View';
+
+const Modal = ({ visible, children }) => {
+  return visible ? (
+    <Portal>
+      <View style={styles.modal}>{children}</View>
+    </Portal>
+  ) : null;
+};
+
+const styles = StyleSheet.create({
+  modal: {
+  }
+});
+
+export default Modal;
